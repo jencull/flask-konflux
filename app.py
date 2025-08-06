730ec8dd87f139e61b80a0839c6f1c7aff3d5c24
Ryan Cole <rcyoalne@gmail.com>
2025-08-06 11:25:12 -0400

    Organize data in .tekton directory
    Organize data to make adding further tests simpler
    
    Signed-off-by: Ryan Cole <rcyoalne@gmail.com>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return "<h1>Happy holidays from Flask & Konflux!</h1>"
+    return "<h1>Hello from Flask & Konflux!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
